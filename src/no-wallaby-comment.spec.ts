1f2f3a3df5846056f8f177072ef3eba7acb46aa3
jagan <jagan.polavarapu@checkdigit.com>
2023-10-24 14:11:24 -0500

    Added new custom eslint rule to fix wallaby comments

---
+++ src/no-wallaby-comment.spec.ts
@@ -0,0 +1,40 @@
+// no-wallaby-comment.spec.ts
+
+/*
+ * Copyright (c) 2022-2023 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+import { RuleTester } from 'eslint';
+
+import rule from './no-wallaby-comment';
+
+const LINE_WITH_NO_COMMENTS = `const NOT_A_SECRET = "A template that isn't a secret.";`;
+
+const LINE_WITH_COMMENT = `const NOT_A_SECRET = "A template that isn't a secret." // ?`;
+
+const LINE_WITH_MULTIPLE_COMMENTS = `// file.only const NOT_A_SECRET = "A template that isn't a secret."; // ?? // ?`;
+
+describe('no-wallaby-comment', () => {
+  const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020 } });
+
+  ruleTester.run('no-wallaby-comment', rule, {
+    valid: [
+      {
+        code: LINE_WITH_NO_COMMENTS,
+      },
+    ],
+    invalid: [
+      {
+        code: LINE_WITH_COMMENT,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: `const NOT_A_SECRET = "A template that isn't a secret."`,
+      },
+      {
+        code: LINE_WITH_MULTIPLE_COMMENTS,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
+      },
+    ],
+  });
+});
---
+++ src/no-wallaby-comment.ts
@@ -0,0 +1,46 @@
+// no-wallaby-comment.ts
+
+/*
+ * Copyright (c) 2022-2023 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+
+import type { Rule } from 'eslint';
+
+function removeComments(input: string) {
+  let modifiedInput = input;
+  let previous: string;
+
+  do {
+    previous = modifiedInput;
+    modifiedInput = modifiedInput.replace(/(?:\/\/|<!--)\s*(?<comment>\?{1,2}\.?|file\.only)/gu, '').trim();
+  } while (modifiedInput !== previous);
+
+  return modifiedInput;
+}
+
+export default {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Detects wallaby-specific temporary comments like // ? or // ?? or // ?. or // file.only and fix it',
+      url: 'https://github.com/checkdigit/eslint-plugin',
+    },
+    fixable: 'code',
+  },
+  create(context: Rule.RuleContext) {
+    const sourceCode = context.sourceCode;
+    const modifiedCode = removeComments(sourceCode.text);
+
+    if (sourceCode.text !== modifiedCode) {
+      context.report({
+        loc: { line: 1, column: 0 },
+        message: 'Remove wallaby-specific comments',
+        fix: (fixer) => fixer.replaceTextRange([0, sourceCode.text.length], modifiedCode),
+      });
+    }
+
+    return {};
+  },
+} as Rule.RuleModule;
