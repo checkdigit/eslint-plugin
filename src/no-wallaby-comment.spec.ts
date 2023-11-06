71d9551acbdd62f9465a8375c9b9287a66b311af
jagan <jagan.polavarapu@checkdigit.com>
2023-11-05 22:06:30 -0600

    Added functionality to handle block type comments with test coverage

--- src/no-wallaby-comment.spec.ts
+++ src/no-wallaby-comment.spec.ts
@@ -9,13 +9,16 @@ import { RuleTester } from 'eslint';
 
 import rule from './no-wallaby-comment';
 
-const LINE_WITH_NO_COMMENTS = `const NOT_A_SECRET = "A template that isn't a secret.";`;
+const LINE_ONE_NO_COMMENTS = `const NOT_A_SECRET = "A template that isn't a secret.";`;
+const LINE_TWO_NO_COMMENTS = `/*
+* This is a comment
+*/`;
 
 const LINE_ONE_WITH_MULTIPLE_COMMENTS = `// file.only 
 const NOT_A_SECRET = "A template that isn't a secret.";
-const NOT_SECRET = "A template that isn't a secret."; // ??.
-const TEST = "this isn't secret"; // ?
-const SECRET = "A template that is a secret."; // ??
+const NOT_SECRET = "A template that isn't a secret.";// ??.
+const TEST = "this isn't secret";// ?
+const SECRET = "A template that is a secret.";// ??
 `;
 
 const LINE_ONE_WITH_MULTIPLE_COMMENTS_EXPECTED = `
@@ -25,12 +28,12 @@ const TEST = "this isn't secret";
 const SECRET = "A template that is a secret.";
 `;
 
-const LINE_TWO_WITH_MULTIPLE_COMMENTS = `// file.skip 
+const LINE_TWO_WITH_MULTIPLE_COMMENTS = `// file.only 
 const NOT_A_SECRET = "A template that isn't a secret";
         const NOT_SECRET = "A template that isn't a secret.";
-        const TEST = "this isn't secret"; // ?
-        const TEST_DOT = "this isn't secret"; // ?.
-        const SECRET = "A template that is a secret."; // ??
+        const TEST = "this isn't secret";// ?
+        const TEST_DOT = "this isn't secret";// ?.
+        const SECRET = "A template that is a secret.";// ??
 const TEST_LINE = "test template";
 `;
 
@@ -43,40 +46,69 @@ const NOT_A_SECRET = "A template that isn't a secret";
 const TEST_LINE = "test template";
 `;
 
+const LINE_THREE_WITH_COMMENTS = `//       file.only`;
+const LINE_FOUR_WITH_COMMENTS = `/* file.only    */`;
+const LINE_FIVE_WITH_COMMENTS = `/*
+* file.only
+*/`;
+
+const LINE_SIX_WITH_COMMENTS = `/*
+* This is first comment
+* This is second comment
+* file.skip
+* This is third comment
+* This is fourth comment
+*/`;
+
+const LINE_SIX_WITH_COMMENTS_EXPECTED = `/*
+* This is first comment
+* This is second comment
+* This is third comment
+* This is fourth comment
+*/`;
+
 describe('no-wallaby-comment', () => {
   const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020 } });
 
   ruleTester.run('no-wallaby-comment', rule, {
     valid: [
       {
-        code: LINE_WITH_NO_COMMENTS,
+        code: LINE_ONE_NO_COMMENTS,
+      },
+      {
+        code: LINE_TWO_NO_COMMENTS,
       },
     ],
     invalid: [
       {
-        code: `const NOT_A_SECRET = "A template that isn't a secret."; // ?`,
+        code: `const NOT_A_SECRET = "A template that isn't a secret.";// ?`,
         errors: [{ message: 'Remove wallaby-specific comments' }],
         output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
       },
       {
-        code: `const NOT_A_SECRET = "A template that isn't a secret."; // ??`,
+        code: `const NOT_A_SECRET = "A template that isn't a secret.";// ??`,
         errors: [{ message: 'Remove wallaby-specific comments' }],
         output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
       },
       {
-        code: `const NOT_A_SECRET = "A template that isn't a secret."; // ?.`,
+        code: `const NOT_A_SECRET = "A template that isn't a secret.";// ?.`,
         errors: [{ message: 'Remove wallaby-specific comments' }],
         output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
       },
       {
-        code: `const NOT_A_SECRET = "A template that isn't a secret."; // ??.`,
+        code: `const NOT_A_SECRET = "A template that isn't a secret.";// ??.`,
         errors: [{ message: 'Remove wallaby-specific comments' }],
         output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
       },
       {
-        code: `// file.only const NOT_A_SECRET = "A template that isn't a secret.";`,
+        code: `// file.only`,
         errors: [{ message: 'Remove wallaby-specific comments' }],
-        output: `const NOT_A_SECRET = "A template that isn't a secret.";`,
+        output: ``,
+      },
+      {
+        code: `// file.skip`,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: ``,
       },
       {
         code: LINE_ONE_WITH_MULTIPLE_COMMENTS,
@@ -97,6 +129,26 @@ describe('no-wallaby-comment', () => {
           { message: 'Remove wallaby-specific comments' },
         ],
         output: LINE_TWO_WITH_MULTIPLE_COMMENTS_EXPECTED,
+      },
+      {
+        code: LINE_THREE_WITH_COMMENTS,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: '',
+      },
+      {
+        code: LINE_FOUR_WITH_COMMENTS,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: '',
+      },
+      {
+        code: LINE_FIVE_WITH_COMMENTS,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: '',
+      },
+      {
+        code: LINE_SIX_WITH_COMMENTS,
+        errors: [{ message: 'Remove wallaby-specific comments' }],
+        output: LINE_SIX_WITH_COMMENTS_EXPECTED,
       },
     ],
   });
--- src/no-wallaby-comment.ts
+++ src/no-wallaby-comment.ts
@@ -6,7 +6,52 @@
  * This code is licensed under the MIT license (see LICENSE.txt for details).
  */
 
-import type { Rule } from 'eslint';
+import type { Rule, SourceCode } from 'eslint';
+import type { Comment } from 'estree';
+
+const wallabyRegex = /\s*(?:[?]{1,2}\.?|file\.only|file\.skip)\s*/gu;
+
+function removeWallabyComment(context: Rule.RuleContext, sourceCode: SourceCode, start: number, end: number): void {
+  context.report({
+    loc: {
+      start: sourceCode.getLocFromIndex(start),
+      end: sourceCode.getLocFromIndex(end),
+    },
+    message: 'Remove wallaby-specific comments',
+    fix: (fixer) => fixer.removeRange([start, end]),
+  });
+}
+
+function processLineComment(context: Rule.RuleContext, sourceCode: SourceCode, comment: Comment): void {
+  while (wallabyRegex.exec(comment.value) !== null) {
+    if (comment.range) {
+      const start = comment.range[0];
+      const end = comment.range[1];
+      removeWallabyComment(context, sourceCode, start, end);
+    }
+  }
+}
+
+function processBlockComment(context: Rule.RuleContext, sourceCode: SourceCode, comment: Comment): void {
+  const commentValues = comment.value.split('\n');
+  const blockCommentRegex = /^(?:\s*\*\s*)?(?:file\.only|file\.skip)$/gu;
+  commentValues.forEach((commentValue) => {
+    let match;
+    while (comment.range && (match = wallabyRegex.exec(commentValue)) !== null) {
+      let start = 0;
+      let end = 0;
+      const removeEntireComment = blockCommentRegex.test(comment.value.trim());
+      if (removeEntireComment) {
+        start = comment.range[0];
+        end = comment.range[1];
+      } else {
+        start = comment.value.indexOf(match.input) + 1;
+        end = comment.value.indexOf(match.input) + match.input.length + 2;
+      }
+      removeWallabyComment(context, sourceCode, start, end);
+    }
+  });
+}
 
 export default {
   meta: {
@@ -20,26 +65,16 @@ export default {
   },
   create(context: Rule.RuleContext) {
     const sourceCode = context.sourceCode;
-    const sourceLines = sourceCode.getLines();
+    const comments = sourceCode.getAllComments();
 
-    sourceLines.forEach((line, lineNumber) => {
-      const regex = /\s*(?:\/\/|<!--)\s*(?<comment>\?{1,2}\.?\s*|file\.(?:only|skip))\s*/gu;
-
-      let match;
-      while ((match = regex.exec(line)) !== null) {
-        const commentStart = match.index;
-        const start = sourceCode.getIndexFromLoc({ line: lineNumber + 1, column: commentStart });
-        const end = sourceCode.getIndexFromLoc({ line: lineNumber + 1, column: commentStart + match[0].length });
-        context.report({
-          loc: {
-            line: lineNumber + 1,
-            column: commentStart,
-          },
-          message: 'Remove wallaby-specific comments',
-          fix: (fixer) => fixer.removeRange([start, end]),
-        });
+    comments.forEach((comment) => {
+      if (comment.type === 'Line') {
+        processLineComment(context, sourceCode, comment);
+      } else {
+        processBlockComment(context, sourceCode, comment);
       }
     });
+
     return {};
   },
 } as Rule.RuleModule;
