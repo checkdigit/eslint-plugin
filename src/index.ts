19576447c420e657ccadc87d6fc7d200939cbb14
jagan <jagan.polavarapu@checkdigit.com>
2023-11-16 12:48:29 -0600

    Installed eslint plugin no-enum

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,12 @@
 import filePathComment from './file-path-comment';
 import noCardNumbers from './no-card-numbers';
 import noUuid from './no-uuid';
-import noEnum from './no-enum';
 
 export default {
   rules: {
     'file-path-comment': filePathComment,
     'no-card-numbers': noCardNumbers,
     'no-uuid': noUuid,
-    'no-enum': noEnum,
   },
   configs: {
     all: {
@@ -24,7 +22,6 @@ export default {
         '@checkdigit/no-card-numbers': 'error',
         '@checkdigit/file-path-comment': 'error',
         '@checkdigit/no-uuid': 'error',
-        '@checkdigit/no-enum': 'error',
       },
     },
     recommended: {
@@ -32,7 +29,6 @@ export default {
         '@checkdigit/no-card-numbers': 'error',
         '@checkdigit/file-path-comment': 'off',
         '@checkdigit/no-uuid': 'error',
-        '@checkdigit/no-enum': 'error',
       },
     },
   },
