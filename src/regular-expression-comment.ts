01acba1392779f0755135867194085d49043b7e6
jagan <jagan.polavarapu@checkdigit.com>
2024-07-03 13:38:53 -0500

    Fixed to allow simple text-only regexs to not have a comment

--- src/regular-expression-comment.ts
+++ src/regular-expression-comment.ts
@@ -25,35 +25,41 @@ export default {
           const regularExpressionLine = node.loc.start.line;
           const previousLine = regularExpressionLine - 1;
           const previousLineComment = lines[previousLine - 1];
-          const regularExpressionComment = comments.find((comment) => {
-            if (!comment.loc) {
-              return false;
-            }
-            // This regex is to check if the comment has valid text
-            const regularExpression = /[a-zA-Z]/gu;
-            // This regex is to check if the line starts with or without spaces and followed by two or more consecutive slashes // or start with /* and may have one or more asterisks, continuing until the first occurrence of */.
-            const commentRegularExpressionLine = /^\s*(?:\/{2,}|\/\*+)/gu;
-            const hasComment = regularExpression.test(comment.value.trim());
-            if (comment.type === 'Line' || comment.loc.start.line === comment.loc.end.line) {
+
+          // Check if the regular expression contains any simple text
+          const isSimpleTextPattern = /^[\w\s.,]*$/gu.test(node.value.source);
+
+          if (!isSimpleTextPattern) {
+            const regularExpressionComment = comments.find((comment) => {
+              if (!comment.loc) {
+                return false;
+              }
+              // This regex is to check if the comment has valid text
+              const regularExpression = /[a-zA-Z]/gu;
+              // This regex is to check if the line starts with or without spaces and followed by two or more consecutive slashes // or start with /* and may have one or more asterisks, continuing until the first occurrence of */.
+              const commentRegularExpressionLine = /^\s*(?:\/{2,}|\/\*+)/gu;
+              const hasComment = regularExpression.test(comment.value.trim());
+              if (comment.type === 'Line' || comment.loc.start.line === comment.loc.end.line) {
+                return (
+                  (comment.loc.end.line === previousLine &&
+                    previousLineComment !== undefined &&
+                    commentRegularExpressionLine.test(previousLineComment) &&
+                    hasComment) ||
+                  (comment.loc.end.line === regularExpressionLine && hasComment)
+                );
+              }
               return (
-                (comment.loc.end.line === previousLine &&
-                  previousLineComment !== undefined &&
-                  commentRegularExpressionLine.test(previousLineComment) &&
-                  hasComment) ||
+                (comment.loc.end.line === previousLine && hasComment) ||
                 (comment.loc.end.line === regularExpressionLine && hasComment)
               );
-            }
-            return (
-              (comment.loc.end.line === previousLine && hasComment) ||
-              (comment.loc.end.line === regularExpressionLine && hasComment)
-            );
-          });
-
-          if (!regularExpressionComment) {
-            context.report({
-              node,
-              message: 'Missing comment for regular expression',
             });
+
+            if (!regularExpressionComment) {
+              context.report({
+                node,
+                message: 'Missing comment for regular expression',
+              });
+            }
           }
         }
       },
