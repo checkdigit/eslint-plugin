96c5f5bae3d2f9518f61e618b27b990bfddb93dd
Le Cong <le.cong@checkdigit.com>
2024-08-01 15:16:09 -0400

    replace statusCode with status

---
+++ src/fixture/no-status-code.spec.ts
@@ -0,0 +1,44 @@
+// fixture/no-status-code.spec.ts
+
+/*
+ * Copyright (c) 2021-2024 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+
+import rule, { ruleId } from './no-status-code';
+import { RuleTester } from '@typescript-eslint/rule-tester';
+
+const ruleTester = new RuleTester({
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: '../tsconfig.json',
+    tsconfigRootDir: `${process.cwd()}/ts-init`,
+  },
+});
+
+ruleTester.run(ruleId, rule, {
+  valid: [
+    {
+      name: 'no change if no "status" property is found in response type',
+      code: `
+        const response = {statusCode: 200};
+        const status = response.statusCode;
+      `,
+    },
+  ],
+  invalid: [
+    {
+      name: 'replace statusCode with status',
+      code: `
+        const response = await fetch(\`https://ping.checkdigit/ping/v1/key/\${keyId}\`);
+        const status = response.statusCode;
+      `,
+      output: `
+        const response = await fetch(\`https://ping.checkdigit/ping/v1/key/\${keyId}\`);
+        const status = response.status;
+      `,
+      errors: [{ messageId: 'replaceStatusCode' }],
+    },
+  ],
+});
---
+++ src/fixture/no-status-code.ts
@@ -0,0 +1,72 @@
+// fixture/no-status-code.ts
+
+/*
+ * Copyright (c) 2021-2024 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+
+import { ESLintUtils, TSESTree } from '@typescript-eslint/utils';
+import getDocumentationUrl from '../get-documentation-url';
+
+export const ruleId = 'no-status-code';
+
+const createRule = ESLintUtils.RuleCreator((name) => getDocumentationUrl(name));
+
+const rule = createRule({
+  name: ruleId,
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'Access the status code property of the fetch Response using "status" instead of "statusCode".',
+    },
+    messages: {
+      replaceStatusCode: 'Replacing "statusCode" with "status".',
+      unknownError: 'Unknown error occurred in file "{{fileName}}": {{ error }}.',
+    },
+    fixable: 'code',
+    schema: [],
+  },
+  defaultOptions: [],
+  // eslint-disable-next-line max-lines-per-function
+  create(context) {
+    const parserServices = ESLintUtils.getParserServices(context);
+    const typeChecker = parserServices.program.getTypeChecker();
+
+    return {
+      'MemberExpression[property.name="statusCode"]': (responseStatusCode: TSESTree.MemberExpression) => {
+        try {
+          const responseNode = parserServices.esTreeNodeToTSNodeMap.get(responseStatusCode.object);
+          const responseType = typeChecker.getTypeAtLocation(responseNode);
+
+          const shouldReplace =
+            responseType.getProperties().some((symbol) => symbol.name === 'status') &&
+            !responseType.getProperties().some((symbol) => symbol.name === 'statusCode');
+
+          if (shouldReplace) {
+            context.report({
+              messageId: 'replaceStatusCode',
+              node: responseStatusCode.property,
+              fix(fixer) {
+                return fixer.replaceText(responseStatusCode.property, 'status');
+              },
+            });
+          }
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(`Failed to apply ${ruleId} rule for file "${context.filename}":`, error);
+          context.report({
+            node: responseStatusCode,
+            messageId: 'unknownError',
+            data: {
+              fileName: context.filename,
+              error: error instanceof Error ? error.toString() : JSON.stringify(error),
+            },
+          });
+        }
+      },
+    };
+  },
+});
+
+export default rule;
