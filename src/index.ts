ff3d4224fc8e7b313c64d8f3935217560b4d990f
Le Cong <le.cong@checkdigit.com>
2024-10-11 11:43:16 -0400

    minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@
  * This code is licensed under the MIT license (see LICENSE.txt for details).
  */
 
-import fs from 'node:fs';
+import type { TSESLint } from '@typescript-eslint/utils';
 
+import { name, version } from '../package.json';
 import invalidJsonStringify, { ruleId as invalidJsonStringifyRuleId } from './invalid-json-stringify';
 import noFullResponse, { ruleId as noFullResponseRuleId } from './agent/no-full-response';
 import noPromiseInstanceMethod, { ruleId as noPromiseInstanceMethodRuleId } from './no-promise-instance-method';
@@ -27,38 +28,32 @@ import regexComment from './regular-expression-comment';
 import requireAssertPredicateRejectsThrows from './require-assert-predicate-rejects-throws';
 import requireStrictAssert from './require-strict-assert';
 
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8')) as {
-  name: string;
-  version: string;
+const rules: Record<string, TSESLint.LooseRuleDefinition> = {
+  'file-path-comment': filePathComment,
+  'no-card-numbers': noCardNumbers,
+  'no-uuid': noUuid,
+  'require-strict-assert': requireStrictAssert,
+  'no-test-import': noTestImport,
+  'no-wallaby-comment': noWallabyComment,
+  'regular-expression-comment': regexComment,
+  'require-assert-predicate-rejects-throws': requireAssertPredicateRejectsThrows,
+  'object-literal-response': objectLiteralResponse,
+  [invalidJsonStringifyRuleId]: invalidJsonStringify,
+  [noPromiseInstanceMethodRuleId]: noPromiseInstanceMethod,
+  [noFullResponseRuleId]: noFullResponse,
+  [requireResolveFullResponseRuleId]: requireResolveFullResponse,
+  [requireTypeOutOfTypeOnlyImportsRuleId]: requireTypeOutOfTypeOnlyImports,
 };
 
-const plugin = {
-  // preferred location of name and version
+const plugin: TSESLint.FlatConfig.Plugin = {
   meta: {
-    name: pkg.name,
-    version: pkg.version,
+    name,
+    version,
   },
-  rules: {
-    'file-path-comment': filePathComment,
-    'no-card-numbers': noCardNumbers,
-    'no-uuid': noUuid,
-    'require-strict-assert': requireStrictAssert,
-    'no-test-import': noTestImport,
-    'no-wallaby-comment': noWallabyComment,
-    'regular-expression-comment': regexComment,
-    'require-assert-predicate-rejects-throws': requireAssertPredicateRejectsThrows,
-    'object-literal-response': objectLiteralResponse,
-    [invalidJsonStringifyRuleId]: invalidJsonStringify,
-    [noPromiseInstanceMethodRuleId]: noPromiseInstanceMethod,
-    [noFullResponseRuleId]: noFullResponse,
-    [requireResolveFullResponseRuleId]: requireResolveFullResponse,
-    [requireTypeOutOfTypeOnlyImportsRuleId]: requireTypeOutOfTypeOnlyImports,
-  },
-  configs: {},
+  rules,
 };
 
-// assign configs here so we can reference `plugin`
-Object.assign(plugin.configs, {
+const configs: Record<string, TSESLint.FlatConfig.Config> = {
   all: {
     plugins: {
       '@checkdigit': plugin,
@@ -98,6 +93,9 @@ Object.assign(plugin.configs, {
       [`@checkdigit/${noPromiseInstanceMethodRuleId}`]: 'error',
     },
   },
-});
+};
 
-export default plugin;
+export default {
+  ...plugin,
+  configs,
+};
