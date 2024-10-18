d54d613fdf3d4e80130e0c7fb6fcd40f5a12ebfa
Le Cong <le.cong@checkdigit.com>
2024-10-18 11:39:33 -0400

    don't autofix no-full-response

--- src/agent/no-full-response.spec.ts
+++ src/agent/no-full-response.spec.ts
@@ -22,29 +22,22 @@ ruleTester.run(ruleId, rule, {
   valid: [],
   invalid: [
     {
-      name: 'remove type annotation from variable declaration',
+      name: 'report type annotation from variable declaration',
       code: `const responses: FullResponse<unknown> = await fixture.api.put('\${BASE_PATH}/ping').send(testCard);`,
-      output: `const responses = await fixture.api.put('\${BASE_PATH}/ping').send(testCard);`,
-      errors: [{ messageId: 'removeFullResponse' }],
+      errors: [{ messageId: 'noFullResponse' }],
     },
     {
-      name: 'remove type annotation from array variable declaration',
+      name: 'report type annotation from array variable declaration',
       code: `
         const results: FullResponse<unknown>[] = await Promise.all([
           fetch(\`\${BASE_PATH}/ping\`),
           fetch(\`\${BASE_PATH}/ping\`)
         ]);
       `,
-      output: `
-        const results = await Promise.all([
-          fetch(\`\${BASE_PATH}/ping\`),
-          fetch(\`\${BASE_PATH}/ping\`)
-        ]);
-      `,
-      errors: [{ messageId: 'removeFullResponse' }],
+      errors: [{ messageId: 'noFullResponse' }],
     },
     {
-      name: 'remove type annotation from function return type',
+      name: 'report type annotation from function return type',
       code: `
         export async function putPersonDataEncryptionKey(
           configuration: Configuration,
@@ -63,37 +56,17 @@ ruleTester.run(ruleId, rule, {
           throw new Error(\`Error creating Person data encryption key \${dataEncryptionKeyId}. \`);
         }
       `,
-      output: `
-        export async function putPersonDataEncryptionKey(
-          configuration: Configuration,
-          inboundContext: InboundContext,
-          dataEncryptionKeyId: string,
-        ) {
-          const putDataEncryptionKeyResponse = await configuration.service
-            .person(inboundContext)
-            .put(\`/person/v1/data-encryption-key/\${dataEncryptionKeyId}\`, requestBody, {
-              resolveWithFullResponse: true,
-            });
-
-          if (putDataEncryptionKeyResponse.statusCode === StatusCodes.OK) {
-            return putDataEncryptionKeyResponse;
-          }
-          throw new Error(\`Error creating Person data encryption key \${dataEncryptionKeyId}. \`);
-        }
-      `,
-      errors: [{ messageId: 'removeFullResponse' }],
+      errors: [{ messageId: 'noFullResponse' }],
     },
     {
-      name: 'remove type annotation from arrow function argument narrowing',
+      name: 'report type annotation from arrow function argument narrowing',
       code: `putResponses.map((putResponse: FullResponse<unknown>) => putResponse.statusCode)`,
-      output: `putResponses.map((putResponse) => putResponse.statusCode)`,
-      errors: [{ messageId: 'removeFullResponse' }],
+      errors: [{ messageId: 'noFullResponse' }],
     },
     {
-      name: 'remove type annotation from "as" type narrowingF',
+      name: 'report type annotation from "as" type narrowing',
       code: `const fullResponse = response as FullResponse<unknown>;`,
-      output: `const fullResponse = response;`,
-      errors: [{ messageId: 'removeFullResponse' }],
+      errors: [{ messageId: 'noFullResponse' }],
     },
   ],
 });
