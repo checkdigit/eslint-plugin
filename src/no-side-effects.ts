997d638e7c58179213a53415aff2b8f5d9ed89d7
jagan <jagan.polavarapu@checkdigit.com>
2025-03-17 10:10:23 -0500

    Updated no-side-effects rule to error on some of Variable Declarations, Control Flows

--- src/no-side-effects.ts
+++ src/no-side-effects.ts
@@ -16,65 +16,78 @@ interface RuleOptions {
 export const ruleId = 'no-side-effects';
 const NO_SIDE_EFFECTS = 'NO_SIDE_EFFECTS';
 
-// Type guard to check if a node is an ExpressionStatement
-function isExpressionStatement(node: TSESTree.Node): node is TSESTree.ExpressionStatement {
-  return node.type === TSESTree.AST_NODE_TYPES.ExpressionStatement;
-}
+// Type guards
 
-// Type guard to check if a node is an AwaitExpression
-function isAwaitExpression(statement: TSESTree.Node): boolean {
-  return isExpressionStatement(statement) && statement.expression.type === TSESTree.AST_NODE_TYPES.AwaitExpression;
-}
+// Checks if a node is an ExpressionStatement
+const isExpressionStatement = (node: TSESTree.Node): node is TSESTree.ExpressionStatement =>
+  node.type === TSESTree.AST_NODE_TYPES.ExpressionStatement;
 
-// To check if it is a call expression with a member expression i.e. module.method()
-function isCallExpressionCalleeMemberExpression(statement: TSESTree.Node, excludedIdentifiers: string[]): boolean {
-  return (
-    isExpressionStatement(statement) &&
-    statement.expression.type === TSESTree.AST_NODE_TYPES.CallExpression &&
-    statement.expression.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
-    statement.expression.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
-    statement.expression.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
-    !excludedIdentifiers.includes(statement.expression.callee.object.name) &&
-    !excludedIdentifiers.includes(
-      `${statement.expression.callee.object.name}.${statement.expression.callee.property.name}`,
-    )
+// Checks if a statement is an AwaitExpression
+const isAwaitExpression = (statement: TSESTree.Node): boolean =>
+  isExpressionStatement(statement) && statement.expression.type === TSESTree.AST_NODE_TYPES.AwaitExpression;
+
+// Checks if a node is a VariableDeclaration with an AwaitExpression
+const isVariableDeclarationAwaitExpression = (node: TSESTree.Node): boolean =>
+  node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration &&
+  node.declarations.length > 0 &&
+  node.declarations[0]?.init?.type === TSESTree.AST_NODE_TYPES.AwaitExpression;
+
+// Checks if a node is a VariableDeclaration that is not const or using
+const isNotValidVariableDeclaration = (node: TSESTree.Node): boolean =>
+  node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration && node.kind !== 'const' && node.kind !== 'using';
+
+// Checks if a node is a VariableDeclaration that is const or using
+const isConstOrUsingVariableDeclaration = (node: TSESTree.Node): boolean =>
+  node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration && (node.kind === 'const' || node.kind === 'using');
+
+// Checks if a node is a control flow statement
+const isControlFlowStatement = (node: TSESTree.Node): boolean =>
+  [
+    TSESTree.AST_NODE_TYPES.TryStatement,
+    TSESTree.AST_NODE_TYPES.IfStatement,
+    TSESTree.AST_NODE_TYPES.SwitchStatement,
+    TSESTree.AST_NODE_TYPES.ForStatement,
+    TSESTree.AST_NODE_TYPES.WhileStatement,
+    TSESTree.AST_NODE_TYPES.DoWhileStatement,
+  ].includes(node.type);
+
+// Checks if a node is an AssignmentExpression
+const isAssignmentExpression = (node: TSESTree.Node): boolean =>
+  node.type === TSESTree.AST_NODE_TYPES.ExpressionStatement &&
+  node.expression.type === TSESTree.AST_NODE_TYPES.AssignmentExpression;
+
+// Helper functions
+
+// Checks if the callee is an identifier and not excluded
+const isIdentifierCallee = (node: TSESTree.CallExpression, excludedIdentifiers: string[]): boolean =>
+  node.callee.type === TSESTree.AST_NODE_TYPES.Identifier && !excludedIdentifiers.includes(node.callee.name);
+
+// Checks if the callee is a member expression and not excluded
+const isMemberExpressionCallee = (node: TSESTree.CallExpression, excludedIdentifiers: string[]): boolean =>
+  node.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
+  node.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
+  node.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
+  !excludedIdentifiers.includes(`${node.callee.object.name}.${node.callee.property.name}`);
+
+// Checks if the callee is a member expression with a non-identifier object
+const isNonIdentifierObjectMemberExpressionCallee = (node: TSESTree.CallExpression): boolean =>
+  node.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
+  node.callee.object.type !== TSESTree.AST_NODE_TYPES.Identifier;
+
+// Checks if a statement is a CallExpression with a member expression callee
+const isCallExpressionCalleeMemberExpression = (statement: TSESTree.Node, excludedIdentifiers: string[]): boolean =>
+  isExpressionStatement(statement) &&
+  statement.expression.type === TSESTree.AST_NODE_TYPES.CallExpression &&
+  statement.expression.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
+  statement.expression.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
+  statement.expression.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
+  !excludedIdentifiers.includes(statement.expression.callee.object.name) &&
+  !excludedIdentifiers.includes(
+    `${statement.expression.callee.object.name}.${statement.expression.callee.property.name}`,
   );
-}
 
-// To check if it is a variable declaration with an await expression i.e. const configuration = await someFunction();
-function isVariableDeclarationAwaitExpression(node: TSESTree.Node): boolean {
-  return (
-    node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration &&
-    node.declarations.length > 0 &&
-    node.declarations[0]?.init?.type === TSESTree.AST_NODE_TYPES.AwaitExpression
-  );
-}
-
-// Helper function to check if the callee is an identifier and not excluded
-function isIdentifierCallee(node: TSESTree.CallExpression, excludedIdentifiers: string[]): boolean {
-  return node.callee.type === TSESTree.AST_NODE_TYPES.Identifier && !excludedIdentifiers.includes(node.callee.name);
-}
-
-// Helper function to check if the callee is a member expression and not excluded
-function isMemberExpressionCallee(node: TSESTree.CallExpression, excludedIdentifiers: string[]): boolean {
-  return (
-    node.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
-    node.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
-    node.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
-    !excludedIdentifiers.includes(`${node.callee.object.name}.${node.callee.property.name}`)
-  );
-}
-
-// Helper function to check if the callee is a member expression with a non-identifier object
-function isNonIdentifierObjectMemberExpressionCallee(node: TSESTree.CallExpression): boolean {
-  return (
-    node.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
-    node.callee.object.type !== TSESTree.AST_NODE_TYPES.Identifier
-  );
-}
-
-// To check if it is a variable declaration with a call expression i.e const configuration = new Class(); or Member Expressions i.e const server = http.createServer();
-function isVariableDeclarationCallExpression(node: TSESTree.Node, excludedIdentifiers: string[]): boolean {
+// Checks if a node is a VariableDeclaration with a CallExpression
+const isVariableDeclarationCallExpression = (node: TSESTree.Node, excludedIdentifiers: string[]): boolean => {
   if (node.type !== TSESTree.AST_NODE_TYPES.VariableDeclaration || node.declarations.length === 0) {
     return false;
   }
@@ -93,66 +106,30 @@ function isVariableDeclarationCallExpression(node: TSESTree.Node, excludedIdenti
     isMemberExpressionCallee(init, excludedIdentifiers) ||
     isNonIdentifierObjectMemberExpressionCallee(init)
   );
-}
+};
 
-// Verifies if the export declaration contains a variable declaration with an await expression or a call expression.
-function isExportNamedDeclarationWithSideEffects(statement: TSESTree.Node, excludedIdentifiers: string[]): boolean {
-  return (
-    statement.type === TSESTree.AST_NODE_TYPES.ExportNamedDeclaration &&
-    statement.declaration !== null &&
-    (isVariableDeclarationAwaitExpression(statement.declaration) ||
-      isVariableDeclarationCallExpression(statement.declaration, excludedIdentifiers))
-  );
-}
+// Checks if an ExportNamedDeclaration has side effects
+const isExportNamedDeclarationWithSideEffects = (statement: TSESTree.Node, excludedIdentifiers: string[]): boolean =>
+  statement.type === TSESTree.AST_NODE_TYPES.ExportNamedDeclaration &&
+  statement.declaration !== null &&
+  (isVariableDeclarationAwaitExpression(statement.declaration) ||
+    isVariableDeclarationCallExpression(statement.declaration, excludedIdentifiers));
 
-// Verifies if the expression statement contains a call expression with an identifier or member expression callee that is not excluded.
-function isExpressionStatementWithSideEffects(statement: TSESTree.Node, excludedIdentifiers: string[]): boolean {
-  return (
-    statement.type === TSESTree.AST_NODE_TYPES.ExpressionStatement &&
-    statement.expression.type === TSESTree.AST_NODE_TYPES.CallExpression &&
-    ((statement.expression.callee.type === TSESTree.AST_NODE_TYPES.Identifier &&
-      !excludedIdentifiers.includes(statement.expression.callee.name)) ||
-      (statement.expression.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
-        statement.expression.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
-        statement.expression.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
-        !excludedIdentifiers.includes(
-          `${statement.expression.callee.object.name}.${statement.expression.callee.property.name}`,
-        )))
-  );
-}
+// Checks if an ExpressionStatement has side effects
+const isExpressionStatementWithSideEffects = (statement: TSESTree.Node, excludedIdentifiers: string[]): boolean =>
+  statement.type === TSESTree.AST_NODE_TYPES.ExpressionStatement &&
+  statement.expression.type === TSESTree.AST_NODE_TYPES.CallExpression &&
+  ((statement.expression.callee.type === TSESTree.AST_NODE_TYPES.Identifier &&
+    !excludedIdentifiers.includes(statement.expression.callee.name)) ||
+    (statement.expression.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
+      statement.expression.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
+      statement.expression.callee.property.type === TSESTree.AST_NODE_TYPES.Identifier &&
+      !excludedIdentifiers.includes(
+        `${statement.expression.callee.object.name}.${statement.expression.callee.property.name}`,
+      )));
 
-// Type guard that checks if a given AST node is a variable declaration and that the variable is not declared with the const keyword
-function isNotValidVariableDeclaration(node: TSESTree.Node): boolean {
-  return node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration && node.kind !== 'const' && node.kind !== 'using';
-}
-
-// Checks if the node is a const or using variable declaration
-function isConstOrUsingVariableDeclaration(node: TSESTree.Node): boolean {
-  return node.type === TSESTree.AST_NODE_TYPES.VariableDeclaration && (node.kind === 'const' || node.kind === 'using');
-}
-
-// Checks if the node is a control flow statement
-function isControlFlowStatement(node: TSESTree.Node): boolean {
-  return (
-    node.type === TSESTree.AST_NODE_TYPES.TryStatement ||
-    node.type === TSESTree.AST_NODE_TYPES.IfStatement ||
-    node.type === TSESTree.AST_NODE_TYPES.SwitchStatement ||
-    node.type === TSESTree.AST_NODE_TYPES.ForStatement ||
-    node.type === TSESTree.AST_NODE_TYPES.WhileStatement ||
-    node.type === TSESTree.AST_NODE_TYPES.DoWhileStatement
-  );
-}
-
-// Checks if the node is an assignment expression
-function isAssignmentExpression(node: TSESTree.Node): boolean {
-  return (
-    node.type === TSESTree.AST_NODE_TYPES.ExpressionStatement &&
-    node.expression.type === TSESTree.AST_NODE_TYPES.AssignmentExpression
-  );
-}
-
-// Checks if the statement has side effects
-function hasSideEffects(statement: TSESTree.Node, excludedIdentifiers: string[]): boolean {
+// Checks if a statement has side effects
+const hasSideEffects = (statement: TSESTree.Node, excludedIdentifiers: string[]): boolean => {
   if (isConstOrUsingVariableDeclaration(statement)) {
     return false;
   }
@@ -168,7 +145,7 @@ function hasSideEffects(statement: TSESTree.Node, excludedIdentifiers: string[])
     isNotValidVariableDeclaration(statement) ||
     isAssignmentExpression(statement)
   );
-}
+};
 
 const createRule: ReturnType<typeof ESLintUtils.RuleCreator> = ESLintUtils.RuleCreator((name) => name);
 
