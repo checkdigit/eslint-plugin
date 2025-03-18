79fa9c746321441403511f2ab5acf100b668b16a
jagan <jagan.polavarapu@checkdigit.com>
2025-03-18 14:08:33 -0500

    Fixed to not auto fix which might cause compile errors

--- src/no-type-assertion-as.ts
+++ src/no-type-assertion-as.ts
@@ -10,32 +10,25 @@ import { AST_NODE_TYPES, ESLintUtils, TSESTree } from '@typescript-eslint/utils'
 
 export const ruleId = 'no-as-type-assertion';
 const NO_AS_TYPE_ASSERTION = 'NO_AS_TYPE_ASSERTION';
-const NO_UNKNOWN_TYPE_ASSERTION = 'NO_UNKNOWN_TYPE_ASSERTION';
 
 const createRule = ESLintUtils.RuleCreator((name) => name);
 
-const rule: ESLintUtils.RuleModule<typeof NO_AS_TYPE_ASSERTION | typeof NO_UNKNOWN_TYPE_ASSERTION> = createRule({
+const rule: ESLintUtils.RuleModule<typeof NO_AS_TYPE_ASSERTION> = createRule({
   name: ruleId,
   meta: {
-    type: 'suggestion',
+    type: 'problem',
     docs: {
       description: 'Disallow the use of `as` type assertions and suggest using `satisfies` instead',
     },
     schema: [],
     messages: {
       [NO_AS_TYPE_ASSERTION]: 'Avoid using `as` type assertions. Use `satisfies` instead.',
-      [NO_UNKNOWN_TYPE_ASSERTION]: 'Avoid using `unknown` type assertions.',
     },
-    fixable: 'code',
   },
   defaultOptions: [],
   create(context) {
     return {
       TSAsExpression(node: TSESTree.TSAsExpression) {
-        const sourceCode = context.sourceCode;
-        const typeAnnotation = sourceCode.getText(node.typeAnnotation);
-        const expression = sourceCode.getText(node.expression);
-
         const isUnknownUnionOrIntersection =
           (node.typeAnnotation.type === AST_NODE_TYPES.TSUnionType ||
             node.typeAnnotation.type === AST_NODE_TYPES.TSIntersectionType) &&
@@ -50,14 +43,13 @@ const rule: ESLintUtils.RuleModule<typeof NO_AS_TYPE_ASSERTION | typeof NO_UNKNO
           if (node.parent.type !== AST_NODE_TYPES.TSAsExpression) {
             context.report({
               node,
-              messageId: NO_UNKNOWN_TYPE_ASSERTION,
+              messageId: NO_AS_TYPE_ASSERTION,
             });
           }
         } else {
           context.report({
             node,
             messageId: NO_AS_TYPE_ASSERTION,
-            fix: (fixer) => fixer.replaceText(node, `${expression} satisfies ${typeAnnotation}`),
           });
         }
       },
