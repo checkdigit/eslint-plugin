6c4a28182b2c982ddadfb46328e1f46c64dafa42
Le Cong <le.cong@checkdigit.com>
2026-06-02 22:51:35 -0400

    update dependencies

--- src/openapi/generate-schema.ts
+++ src/openapi/generate-schema.ts
@@ -95,6 +95,7 @@ function getRequestParametersSchema(
   const parametersSchema = Object.fromEntries(
     parameters.map((parameter) => [
       parameterType === 'header' ? parameter.name.toLowerCase() : parameter.name,
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
       parameter.schema ?? ({ type: 'string' } as v31.SchemaObject),
     ]),
   );
@@ -202,6 +203,7 @@ function getResponseHeadersSchema(
   const resolvedHeaderSchemas = Object.fromEntries(
     Object.entries(resolvedHeaders).map(([name, header]) => [
       name,
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
       header.schema ?? ({ type: 'string' } as v31.SchemaObject),
     ]),
   );
