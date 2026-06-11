06ef1410661bbf7045d782e758ae3647602cffaf
Le Cong <le.cong@checkdigit.com>
2026-06-10 22:17:36 -0400

    refresh local schema every hour to avoid stale schema

--- src/athena/api-locator.ts
+++ src/athena/api-locator.ts
@@ -10,6 +10,8 @@ const log = debug('eslint-plugin:athena:api-locator');
 
 const SERVICES_ROOT_FOLDER = 'src/services';
 const LEGACY_TABLE_SUFFIX = '_logs';
+// eslint-disable-next-line no-magic-numbers
+const SCHEMA_MAX_AGE_MS = 60 * 60 * 1000; // 1 hour
 
 function upperCaseFirstCharacter(value: string): string {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
@@ -34,13 +36,25 @@ export function locateApi(originalServiceName: string): ApiSchemas[] {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   log(`${allSchemaFilenames.length} versions of API schemas located for service ${serviceName}`, allSchemaFilenames);
 
+  const outputDir = `${SERVICES_ROOT_FOLDER}/${camelCaseServiceName}`;
+
   if (allSchemaFilenames.length > 0) {
+    const hasStaleSchema = allSchemaFilenames.some(
+      (schemaFilename) => Date.now() - fs.statSync(schemaFilename).mtimeMs > SCHEMA_MAX_AGE_MS,
+    );
+    if (hasStaleSchema) {
+      log('cached schema(s) are stale (older than 1 hour), regenerating for service', serviceName);
+      const regenerated = generateSchemasForService(serviceName, outputDir);
+      if (regenerated.length > 0) {
+        return regenerated.map(({ schema }) => schema);
+      }
+      log('regeneration failed, falling back to stale cached schemas for service', serviceName);
+    }
     return allSchemaFilenames.map(
       (schemaFilename) => JSON.parse(fs.readFileSync(schemaFilename, 'utf-8')) as ApiSchemas,
     );
   }
 
   log('no pre-generated schemas found, attempting on-demand generation for service', serviceName);
-  const outputDir = `${SERVICES_ROOT_FOLDER}/${camelCaseServiceName}`;
   return generateSchemasForService(serviceName, outputDir).map(({ schema }) => schema);
 }
